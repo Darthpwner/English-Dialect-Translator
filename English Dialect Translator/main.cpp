db08ab74e40f751f3087f21781b5638dd37e403b
Matthew Allen Lin <Darthpwner@Matthews-MacBook-Pro.local>
2015-09-25 20:34:56 -0700

    Added all the files

---
+++ English Dialect Translator/main.cpp
@@ -0,0 +1,93 @@
+//
+//  main.cpp
+//  English Dialect Translator
+//
+//  Created by Matthew Allen Lin on 9/25/15.
+//  Copyright © 2015 MAL Software. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//New Yorker Dialect
+string translateTextToNewYorkerDialect(string text);
+bool checkPreviousCharacterIsVowel(int i, string text);
+
+//Nor Cal Dialect
+string translateTextToNorCalDialect(string text);
+void insertHella(int i, string &text);
+
+int main(int argc, const char * argv[]) {
+    string text = "", translation = "";
+    int dialect = -1;
+    
+    while(true) {
+        //cout << "Select the dialect you want to translate\n1) New Yorker\n2) Nor Cal\n";
+        //cin >> dialect;
+        
+        cout << "Enter the text you want to translate: ";
+        getline(cin, text);
+        
+        //Translate the text
+        //if(dialect == 1) {
+        translation = translateTextToNewYorkerDialect(text);
+        //} else if (dialect == 2) {
+        //  translation = translateTextToNorCalDialect(text);
+        //}
+        cout << "The translated text is: " << translation << endl;
+    }
+    
+    return 0;
+}
+
+
+string translateTextToNewYorkerDialect(string text) {
+    for(int i = 1; i < text.length(); i++) {
+        if(text[i] == 'r' && checkPreviousCharacterIsVowel(i, text)) {
+            text[i] = 'h';
+        }
+    }
+    string translation = text;
+    return translation;
+}
+
+string translateTextToNorCalDialect(string text) {
+    int i = 0;
+    while(i < text.length()) {
+        if (text[i] == 'r' && text[i + 1] == 'e' && text[i + 2] == 'a' && text[i + 3] == 'l' && text[i + 4] == 'l' && text[i + 5] == 'y') {
+            for(int j = i; j < text.length(); j++) {
+                text[i] = text[i + 1];  //Move everything back one
+            }
+            insertHella(i, text);
+            i += 6;
+            
+        } else if(text[i] == 'v' && text[i + 1] == 'i' && text[i + 2] == 'e' && text [i + 3] == 'w') {
+            for(int j = i; j < text.length(); j++) {
+                text[i + 1] = text[i];  //Move everything forward one
+            }
+            insertHella(i, text);
+            i += 6;
+        }
+    }
+    
+    string translation = text;
+    return translation;
+}
+
+void insertHella(int i, string &text) {
+    text[i] = 'h';
+    text[i + 1] = 'e';
+    text[i + 2] = 'l';
+    text[i + 3] = 'l';
+    text[i + 4] = 'a';
+}
+
+bool checkPreviousCharacterIsVowel(int i, string text) {
+    if (text[i - 1] == 'a' || text[i - 1] == 'e' || text[i - 1] == 'i' ||
+        text[i - 1] == 'o' || text[i - 1] == 'u') {
+        return true;
+    }
+    return false;
+}
