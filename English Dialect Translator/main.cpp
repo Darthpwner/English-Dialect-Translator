4ddc67d6d52ec0999f4fb4913bbd197eb43528b2
Matthew Allen Lin <Darthpwner@Matthews-MacBook-Pro.local>
2015-09-25 21:09:02 -0700

    Fix parsing bugs for 'a'

--- English Dialect Translator/main.cpp
+++ English Dialect Translator/main.cpp
@@ -42,12 +42,33 @@ int main(int argc, const char * argv[]) {
     return 0;
 }
 
+//store 2
+//put a into 2
+//increment
+//store 3
+//put store 2 into 3
+//increment
+
+//cal -> cawl
+//012 -> 0123
 
 string translateTextToNewYorkerDialect(string text) {
     for(int i = 1; i < text.length(); i++) {
         if(text[i] == 'r' && checkPreviousCharacterIsVowel(i, text)) {
             text[i] = 'h';
-        }
+        } else if(text[i] == 'o' && text[i + 1] != 'u') {
+            text[i] = 'u';
+        } else if(text[i] == 't' && text[i - 1] != 'i') {
+            text[i] = 'd';
+        } /*else if(text[i] == 'a') {
+//            text.resize(text.length() + 1);
+//            for(int j = i; j < text.length() + 1; j++) {
+//                text[j + 1] = text[j];  //Move everything forward one
+//            }
+//            
+//            text[i + 1] = 'w';
+//            continue;
+//        }*/
     }
     string translation = text;
     return translation;
@@ -64,9 +85,10 @@ string translateTextToNorCalDialect(string text) {
             i += 6;
             
         } else if(text[i] == 'v' && text[i + 1] == 'i' && text[i + 2] == 'e' && text [i + 3] == 'w') {
-            for(int j = i; j < text.length(); j++) {
+            for(int j = i; j < text.length() + 1; j++) {
                 text[i + 1] = text[i];  //Move everything forward one
             }
+            text.resize(text.length() + 1);
             insertHella(i, text);
             i += 6;
         }
